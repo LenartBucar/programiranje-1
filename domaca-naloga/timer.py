995c9fc370e8d2b9e58d9f310a48dbd45680920c
LenartBucar <lenart.bucar@student.fmf.uni-lj.si>
2021-12-31 21:43:44 +0100

    Add a timing script

---
+++ domaca-naloga/timer.py
@@ -0,0 +1,20 @@
+import subprocess
+import re
+
+def get_time(output):
+    ptrn = re.compile(r"(\d+\.\d+) s\.")
+    return float(ptrn.search(output).groups()[0])
+
+
+def time(command):
+    out = subprocess.check_output(command)
+    return str(out)
+    
+
+def repeat(command, num):
+    return sum(get_time(time(command)) for _ in range(num)) / num
+
+if __name__ == "__main__":
+    # sudoku = r"sudokuji\newman\whats_in_the_box.sdk"
+    sudoku = r"sudokuji\kletka-1.sdk"
+    print(repeat(["sudoku.exe", sudoku], 10))
